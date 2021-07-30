9b7e65e64a036ee52c3175a871075daae0851c89
Matheus Jacobina <71132039+matheusjacobina-tech@users.noreply.github.com>
2021-07-30 00:16:55 -0300

    feature test

--- src/main/java/com/bootcamp/beerapi/controller/BeerController.java
+++ src/main/java/com/bootcamp/beerapi/controller/BeerController.java
@@ -2,10 +2,17 @@ package com.bootcamp.beerapi.controller;
 
 import lombok.AllArgsConstructor;
 import com.bootcamp.beerapi.dto.BeerDTO;
+import com.bootcamp.beerapi.dto.QuantityDTO;
 import com.bootcamp.beerapi.exception.BeerAlreadyRegisteredException;
+import com.bootcamp.beerapi.exception.BeerNotFoundException;
+import com.bootcamp.beerapi.exception.BeerStockExceededException;
 import com.bootcamp.beerapi.service.BeerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +20,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/beer")
+@RequestMapping("/api/v1/beers")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class BeerController implements BeerControllerDocs {
 
@@ -25,5 +33,26 @@ public class BeerController implements BeerControllerDocs {
     @ResponseStatus(HttpStatus.CREATED)
     public BeerDTO createBeer(@RequestBody @Valid BeerDTO beerDTO) throws BeerAlreadyRegisteredException {
         return beerService.createBeer(beerDTO);
+    }
+
+    @GetMapping("/{name}")
+    public BeerDTO findByName(@PathVariable String name) throws BeerNotFoundException {
+        return beerService.findByName(name);
+    }
+
+    @GetMapping
+    public List<BeerDTO> listBeers() {
+        return beerService.listAll();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws BeerNotFoundException {
+        beerService.deleteById(id);
+    }
+
+    @PatchMapping("/{id}/increment")
+    public BeerDTO increment(@PathVariable Long id, @RequestBody @Valid QuantityDTO quantityDTO) throws BeerNotFoundException, BeerStockExceededException {
+        return beerService.increment(id, quantityDTO.getQuantity());
     }
 }
--- src/test/java/com/bootcamp/beerapi/builder/BeerDTOBuilder.java
+++ src/test/java/com/bootcamp/beerapi/builder/BeerDTOBuilder.java
@@ -1,5 +1,9 @@
 package com.bootcamp.beerapi.builder;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.Builder;
 import com.bootcamp.beerapi.dto.BeerDTO;
 import com.bootcamp.beerapi.enums.BeerType;
@@ -17,7 +21,7 @@ public class BeerDTOBuilder {
     private String brand = "Ambev";
 
     @Builder.Default
-    private int max = 100;
+    private int max = 50;
 
     @Builder.Default
     private int quantity = 10;
